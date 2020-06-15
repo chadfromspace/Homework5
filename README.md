053a8d05df52be71e5d5a1d26cedfcff33fe7007
imac <chadmdubay@gmail.com>
2020-06-15 17:59:17 -0400

    Final code review.

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 ---  
 ## An incredible day planner designed to change your life and eliminate procrastination forever.  
 ---  
-![A Day Planner in the Life](./img.png)  
+![A Day Planner in the Life](img.png)  
 ---  
-[The Greatest Day Planner of All Time](https://chadfromspace.github.io/Homework5/)
+[The Greatest Day Planner of All Time](https://chadfromspace.github.io/Homework5)
 ---  
 *Vivid colors.
 *Incredible text saving ability.
 *Absolutely not a product that already exists.
+*Much better than Notepad because it has colors.
+*Capable of telling you the time with an accuracy of within one hour.
+*Cutting edge technology used to accomplish the same thing as a pencil and a piece of paper.
+*It has colors.
 ---
--- script.js
+++ script.js
@@ -1,45 +1,55 @@
 $(document).ready(function(){
-
+    //Variables to define the current time.
     var currentHour = moment().format("HH");
     var currentHourInt = parseInt(currentHour);
-    var newForm = document.createElement("form");
-    var inputArray = [];
-    $(".container").append(newForm);
+    //Variable to define the current day.
     $("#currentDay").html(moment().format("dddd") + ", " + moment().format("MMMM Do"));
+    //Creating a new form.
+    var newForm = document.createElement("form");
+    //Variable to store form information to localStorage as an array.
+    var inputArray = [];
+    //Creating saveInfo key in local storage.
+    localStorage.getItem("savedInfo");
+    //Appending newForm to the first item of the container class.
+    $(".container").append(newForm);
+    //On submit function for the page form.
     $(newForm).on("submit",function(event){
+        //Preventing page reload on form submission.
         event.preventDefault();
-        inputArray = [];
+        //Clearing inputArray and local storage.
+        inputArray = [];        
         localStorage.clear();
+        //For loop to push the value of all the input boxes on the page to inputArray.
         for(i=0;i<18;i+=2){
             inputArray.push(event.target[i].value);
         }
-        for(i=0;i<9;i++){
-            localStorage.setItem(i,inputArray[i]);
-        }        
+        //Saving inputArray to the savedInfo key.
+        localStorage.setItem("savedInfo",inputArray);        
     })
+    //For loop to create page content.
     for(i=0;i<9;i++){
-        var x = -1;
+        //Y variable set to compare i into the time next to the form item.
         var y = i+9
+        //Creating new elements to append to the page.
         var newDiv = document.createElement("div");
         var newInput = document.createElement("input");
         var newLabel = document.createElement("label");
         var newButton = document.createElement("button");
-        var currentHourValue;
-        if(localStorage.getItem(0)!==null){
-            newInput.value = localStorage.getItem(i);
-        }
+        //Setting the value of the input box text to the corresponding array value
+        newInput.value = localStorage.getItem("savedInfo").split(",")[i];
+        //Adding class designations to the created items.
         $(newDiv).addClass("row");
         $(newInput).addClass("textBox col-sm");
         $(newLabel).addClass("col-sm-1 scheduleLabel");
         $(newButton).addClass("btn btn-dark col-sm-1 saveButton");
         $(newButton).html("Save");
+        //Conditions to set the AM/PM label of the form item.
         if(i<4){
             $(newLabel).html(i+9+"AM");
-            currentHourValue = y;
         } else if(i>3){
             $(newLabel).html(i-3+"PM");
-            currentHourValue = i-3;
         }
+        //Conditions to set the background color of the input box.
         if(y>currentHourInt){
             $(newInput).css("background-color","rgb(119, 10, 10)");
         }
@@ -49,6 +59,7 @@ $(document).ready(function(){
         if(y===currentHourInt){
             $(newInput).css("background-color","rgb(100, 67, 161)");
         }
+        //Appending newDiv to newForm, and then newLabel, newInput, and newButton to newDiv.
         $(newForm).append(newDiv);
         $(newDiv).append(newLabel);
         $(newDiv).append(newInput);
